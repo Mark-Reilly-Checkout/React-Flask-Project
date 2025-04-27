3595907cc7c31d7aebc33edc1c2732ab59d21351
Mark Reilly <mark.reilly@checkout.com>
2025-04-27 20:20:46 +0100

    Forgot useState import

--- frontend/src/components/ApplePay.js
+++ frontend/src/components/ApplePay.js
@@ -1,5 +1,5 @@
 /* global ApplePaySession */
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 
 const ApplePay = () => {
