7fd3fb75b31060ac23dfb63872c1801ba75cf6cc
Mark Reilly <mark.reilly@checkout.com>
2025-05-08 10:22:33 +0100

    Adding test layout page

--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -4,10 +4,25 @@ import GooglePayButton from '@google-pay/button-react';
 const Test = () => {
 
     return (
-        <div class="flex flex-row">
-        <div class="basis-64">01</div>
-        <div class="basis-64">02</div>
-        <div class="basis-128">03</div>
+        <div class="bg-white dark:bg-gray-800 flex justify-center items-center w-screen h-screen p-5">
+        <div class="border shadow-teal-300 shadow-md max-w-2xl p-6 rounded-lg dark:bg-gray-700 dark:text-gray-300">
+          <h1 class="text-4xl font-mono font-extrabold py-3">Tailwind Playground</h1>
+          <ul class="list-disc text-lg px-6">
+            <div class="px-2">
+        <div class="flex -mx-2">
+          <div class="w-1/2 px-2">
+            <div class="bg-gray-400 h-12"></div>
+          </div>
+          <div class="w-1/2 px-2">
+            <div class="bg-gray-400 h-12"></div>
+          </div>
+        </div>
+      </div>
+            <li>Everything here works just like it does when you're running Tailwind CSS locally.</li>
+            <li>The preview updates in real-time as you code.</li>
+            <li>Save and share your components with the community.</li>
+          </ul>
+        </div>
       </div>
     );
 };
