21130efd79a46806384ad826e08410ceaaa464bb
Mark Reilly <mark.reilly@checkout.com>
2025-06-17 15:49:30 +0100

    Adding PayPal API Only integration

--- frontend/src/components/PayPal.js
+++ frontend/src/components/PayPal.js
@@ -18,7 +18,6 @@ const defaultConfig = {
     failureUrl: 'https://react-flask-project-kpyi.onrender.com/failure', // Your failure redirect URL (adjust for deployment)
     disableFunding: 'credit,card,sepa,bancontact,blik,eps,giropay,ideal,mercadopago,mybank,p24,sofort',
     enableFunding: '', // 'paylater' to enable PayPal Pay Later
-    payment_type: 'Recurring'
 };
 
 // All possible commit types for dropdown
@@ -76,7 +75,7 @@ const PayPal = () => {
                 currency: config.currency,
                 amount: Math.round(parseFloat(config.amount) * 100),
                 capture: config.capture,
-                payment_type: config.payment_type,
+                payment_type: "Recurring",
                 items: [{
                     "name": "Wireless Headphones", // Example item, adjust as needed or make configurable
                     "unit_price": Math.round(parseFloat(config.amount) * 100),
