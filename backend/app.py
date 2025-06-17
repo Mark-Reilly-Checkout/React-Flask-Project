800b541481e0476b6a0ea9b8b953240b1df24c5a
Mark Reilly <mark.reilly@checkout.com>
2025-06-17 09:12:52 +0100

    Added payment type to flow

--- backend/app.py
+++ backend/app.py
@@ -82,7 +82,7 @@ def create_payment_session():
         email = data.get("email") # Get email from frontend, no default here as frontend should send it
         country = data.get("country") # Get country from frontend
         currency = data.get("currency") # Get currency from frontend
-        paymentType = data.get("payment_type", "Regular") # Get payment type from frontend, default to "Regular"
+        paymentType = data.get("paymentType", "Regular") # Get payment type from frontend, default to "Regular"
         # Get billing_address from frontend. It will be a dict.
         billing_address_from_frontend = data.get("billing_address")
 
